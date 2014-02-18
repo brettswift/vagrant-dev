15621e6dc89bf7a4576faa9d134cfc11409fbbf1
Brett Swift <brettswift@gmail.com>
2014-02-18 09:16:30 -0700

    fixing permissions on file

--- dist/gnomedesktop/manifests/terminal.pp
+++ dist/gnomedesktop/manifests/terminal.pp
@@ -6,13 +6,14 @@ class gnomedesktop::terminal (
     ensure  => present,
     source  => 'puppet:///modules/gnomedesktop/solarized.sh',
     owner   => $user_name,
-    mode    => 0644,
+    mode    => 0777,
   }
 
   exec { "install solarized":
-    command => "/bin/bash /home/$user_name/solarized.sh  2> /home/$user_name/solarized-stderr.log 1>> /home/$user_name/solarized-install.log",
+    command => "/bin/bash ./solarized.sh  2> solarized-stderr.log 1>> solarized-install.log",
     creates => "/home/$user_name/solarized-install.log",
-    user    => $user_name,
+    cwd     => "/home/$user_name",
+    user    => "$user_name",
   }
 
   File["/home/$user_name/solarized.sh"] ->
