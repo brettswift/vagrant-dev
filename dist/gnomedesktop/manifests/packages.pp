3ddff23029e29b3d8244d47154c4a9182bc721a0
Brett Swift <brettswift@gmail.com>
2014-03-12 15:42:04 -0600

    last commit before nuking the other remote

--- dist/gnomedesktop/manifests/packages.pp
+++ dist/gnomedesktop/manifests/packages.pp
@@ -13,12 +13,17 @@ class gnomedesktop::packages ()  {
   exec { 'groupInstall xWindow':
     unless  => '/usr/bin/yum grouplist "X Window System" | /bin/grep "^Installed Groups"',
     command => '/usr/bin/yum -y groupinstall "X Window System"',
-    notify  => Service["network"],
+    notify  => [
+                Service["network"],
+                Exec['recompile guest additions']
+              ], 
+    timeout => 0
   }
 
   exec { 'groupInstall Desktop':
     unless  => '/usr/bin/yum grouplist "Desktop" | /bin/grep "^Installed Groups"',
-    command => '/usr/bin/yum -y groupinstall "Desktop"',
+    command => '/usr/bin/yum -y groupinstall "Desktop"', 
+    timeout => 0
   }
 
   exec { 'groupInstall Desktop Platform':
@@ -31,9 +36,15 @@ class gnomedesktop::packages ()  {
     command => '/usr/bin/yum -y groupinstall "Fonts"',
   }
 
-  exec { 'groupInstall Internet Browser':
-    unless  => '/usr/bin/yum grouplist "Internet Browser" | /bin/grep "^Installed Groups"',
-    command => '/usr/bin/yum -y groupinstall "Internet Browser"',
+  # exec { 'groupInstall Internet Browser':
+  #   unless  => '/usr/bin/yum grouplist "Internet Browser" | /bin/grep "^Installed Groups"',
+  #   command => '/usr/bin/yum -y groupinstall "Internet Browser"',
+  # }
+
+  exec { 'recompile guest additions':
+    command => '/etc/init.d/vboxadd setup'
   }
+
+  
 
 }
